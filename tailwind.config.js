dca73b7b5ca2f52107f82fe321fd036a0b924eb3
shakil hossain <shakil@meghops.com>
2024-07-06 10:40:17 +0600

    service pages

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,7 @@ const colors = require('tailwindcss/colors');
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./*.{html,js}"],
+  content: ["./*.{html,js}", "./services/*.html", "./products/*.html"],
   theme: {
     colors: {
       ...colors,
